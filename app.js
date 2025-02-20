c6eb259c0803ce42d725e5bbc32806f4fa21fcfb
Cyberhunter <ryt.impresiones@gmail.com>
2025-02-20 09:44:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-// Matríz para guardar los nombres
 let amigos = [];
 let conteo=1;
 
-// Función para agregar un nombre a la lista
 function agregarAmigo() {
     let nombreInput = document.getElementById('amigo');
     let nombre = nombreInput.value.trim();
@@ -21,17 +19,11 @@ function agregarAmigo() {
         return;
     }
 
-    // Añadir el nombre a la matríz
     amigos.push(nombre);
-
-    // Limpia la caja
     nombreInput.value = "";
-
-    // Actualizar la lista visible
     actualizarLista();
 }
 
-// Función para actualizar la lista de nombres en la página
 function actualizarLista() {
     const listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = ""; // Limpia la lista actual
@@ -43,18 +35,14 @@ function actualizarLista() {
     });
 }
 
-// Función para sortear un amigo secreto
 function sortearAmigo() {
     if (amigos.length === 0) {
         alert("¡La lista está vacía! Agrega algunos amigos.");
         return;
     }
-
-    // Selecciona aleatoriamente un amigo
     let sorteo = Math.floor(Math.random() * amigos.length);
     let amigoSorteado = amigos[sorteo];
-   
-    // Imprimir el resultado
+
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<li>El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;             
         
